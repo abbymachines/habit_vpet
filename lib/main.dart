c5a2c14efea87647e1d053732474540070aa617c
abbymachines <abby.machines@gmail.com>
2023-07-24 22:58:21 -0700

    define GradientContainer widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 73, 229, 253),
-                Color.fromARGB(255, 188, 233, 255),
-              ],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'your worm is a happy worm',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
 }
 
-// THE TAMAGOTCHI
-
-// the tamagotchi will have a value for "hunger"
-// this will be a value in the range of 0 to 4.
-
-// it will also have a hidden value for "time"
-// this will be a 3-point scale that gets incremented
-// through over a period of time. each point represents a
-// day at that value. as the time increments,
-// the condition of the 
-
-// the habit trackers
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 73, 229, 253),
+            Color.fromARGB(255, 188, 233, 255),
+          ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'your worm is a happy worm',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
