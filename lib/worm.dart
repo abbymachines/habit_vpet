f10bb191acc052d6b2b703abe0426e7a2f5fc64c
abbymachines <abby.machines@gmail.com>
2023-07-26 11:59:40 -0700

    add worm image to worm widget

--- lib/worm.dart
+++ lib/worm.dart
@@ -5,8 +5,6 @@ class Worm extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Text('(◉‿◉)~~~~~~~~~',
-        style:
-            TextStyle(color: Color.fromARGB(222, 242, 156, 242), fontSize: 28));
+    return Image.asset('assets/images/worm2.png');
   }
 }
