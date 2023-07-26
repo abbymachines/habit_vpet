19e673d474646f36d4609159b9a66a8f6933f159
abbymachines <abby.machines@gmail.com>
2023-07-25 18:15:23 -0700

    turn startAlignment and endAlignment into const variables inside gradient_container.dart

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:habit_vpet/styled_text.dart';
 import 'package:habit_vpet/worm.dart';
-// import 'package:habit_vpet/heart.dart';
 import 'package:habit_vpet/heart_bar.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -13,9 +12,9 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 19, 52, 5),
             Color.fromARGB(255, 209, 243, 17)
           ],
