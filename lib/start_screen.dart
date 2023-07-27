018c985ccc455c93ca78ced3adff42bc791b4e3c
abbymachines <abby.machines@gmail.com>
2023-07-27 16:24:25 -0700

    re-implement Feed Pet button without proper styling

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_vpet/styled_text.dart';
 import 'package:habit_vpet/heart_bar.dart';
+import 'package:habit_vpet/pet_feeder.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startHabits, {super.key});
@@ -14,10 +15,13 @@ class StartScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           const StyledText('your worm is a happy worm'),
-          const SizedBox(height: 80),
+          const SizedBox(height: 20),
           const HeartBar(),
-          Image.asset('assets/images/worm1.png', width: 200),
-          const SizedBox(height: 70),
+          const SizedBox(height: 10),
+          const Center(
+            child: PetFeeder(),
+          ),
+          const SizedBox(height: 30),
           OutlinedButton.icon(
             onPressed: startHabits,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
