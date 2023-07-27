9eca5f84f920eae4d06f862bbee2784591094f86
abbymachines <abby.machines@gmail.com>
2023-07-27 15:21:35 -0700

    set button data via variables pulling in information from questions.dart

--- lib/habits_screen.dart
+++ lib/habits_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habit_vpet/habit_button.dart';
+import 'package:habit_vpet/data/questions.dart';
 
 class HabitsScreen extends StatefulWidget {
   const HabitsScreen({super.key});
@@ -13,19 +14,21 @@ class HabitsScreen extends StatefulWidget {
 class _HabitsScreenState extends State<HabitsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The habit question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          HabitButton(answerText: 'Habit 1', onTap: () {}),
-          HabitButton(answerText: 'Habit 2', onTap: () {}),
-          HabitButton(answerText: 'Habit 3', onTap: () {}),
+          HabitButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          HabitButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          HabitButton(answerText: currentQuestion.answers[2], onTap: () {}),
         ],
       ),
     );
