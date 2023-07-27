50fe932eddac18491bf281997edf023f2b5a3421
abbymachines <abby.machines@gmail.com>
2023-07-27 12:31:04 -0700

    add dummy data to questions.dart

---
+++ lib/data/questions.dart
@@ -0,0 +1,18 @@
+import 'package:habit_vpet/models/habit_question.dart';
+
+const questions = [
+  HabitQuestion(
+    'Did you walk the dog?',
+    ['Yes', 'No', 'Whomst dogge'],
+  ),
+  HabitQuestion(
+    'Did you do your homework?',
+    ['Yes', 'No', 'Youre not tha boss of me..'],
+  ),
+  HabitQuestion(
+    'Did you exercise?',
+    ['Yes', 'No', 'I\'m busy!!!!'],
+  ),
+  HabitQuestion(
+      'Did you clean your room?', ['Yes', 'No', 'shout out to bedroom'])
+];
