1f5a8abcebe98a3dfed819160b43258529dc37a5
abbymachines <abby.machines@gmail.com>
2023-07-26 17:27:35 -0700

    add habits screen widget

---
+++ lib/habits_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HabitsScreen extends StatefulWidget {
+  const HabitsScreen({super.key});
+
+  @override
+  State<HabitsScreen> createState() {
+    return _HabitsScreenState();
+  }
+}
+
+class _HabitsScreenState extends State<HabitsScreen> {
+  @override
+  build(context) {
+    return const Text('HabitsScreen');
+  }
+}
