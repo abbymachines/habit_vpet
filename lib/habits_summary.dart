3d2b3b062a8515f9b8cf66c450ea959036743f36
abbymachines <abby.machines@gmail.com>
2023-07-28 17:02:21 -0700

    wrap habit summary column in SingleChildScrollView widget

--- lib/habits_summary.dart
+++ lib/habits_summary.dart
@@ -9,30 +9,32 @@ class HabitsSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: Column(
-        children: summaryData.map(
-          (data) {
-            return Row(
-              children: [
-                Text(
-                  (((data['question_index'] as int) + 1).toString()),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
-                    ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(
+                    (((data['question_index'] as int) + 1).toString()),
                   ),
-                )
-              ],
-            );
-          },
-        ).toList(),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data['question'] as String),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(data['user_answer'] as String),
+                        Text(data['correct_answer'] as String),
+                      ],
+                    ),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }
