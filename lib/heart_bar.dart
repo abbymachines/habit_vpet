1271761b863561161c773c89b8aeb6cd7c4e6154
abbymachines <abby.machines@gmail.com>
2023-07-30 19:31:12 -0700

    implement heart_bar stateless widget that accepts a positional argument

--- lib/heart_bar.dart
+++ lib/heart_bar.dart
@@ -4,44 +4,25 @@ const fullHeart = '❤️';
 const halfHeart = '❤️‍🩹';
 const emptyHeart = '♡';
 
-class HeartBar extends StatefulWidget {
-  const HeartBar({
-    super.key,
-    required this.health,
-  });
+final heartBarStates = {
+  0: emptyHeart,
+  1: fullHeart,
+  2: fullHeart + fullHeart,
+  3: fullHeart + fullHeart + fullHeart,
+  4: fullHeart + fullHeart + fullHeart + fullHeart,
+};
+
+class HeartBar extends StatelessWidget {
+  const HeartBar(this.health, {super.key});
 
   final int health;
-  final heartBarStates = const {
-    0: emptyHeart,
-    1: fullHeart,
-    2: fullHeart + fullHeart,
-    3: fullHeart + fullHeart + fullHeart,
-    4: fullHeart + fullHeart + fullHeart + fullHeart,
-  };
-
-  @override
-  State<HeartBar> createState() {
-    return _HeartBarState();
-  }
-}
-
-class _HeartBarState extends State<HeartBar> {
-  var hearts = 2;
-
-  void setHealth() {
-    setState(() {
-      hearts = widget.health * 1;
-    });
-  }
 
   @override
   Widget build(context) {
-    final heartBar = widget.heartBarStates[hearts];
-
     return Container(
       alignment: Alignment.center,
       child: Text(
-        heartBar!,
+        heartBarStates[health]!,
         style: const TextStyle(
           fontSize: 28,
         ),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -16,7 +16,7 @@ class StartScreen extends StatelessWidget {
         children: [
           const StyledText('your worm is a happy worm'),
           const SizedBox(height: 20),
-          const HeartBar(health: 3),
+          const HeartBar(3),
           const SizedBox(height: 10),
           const Center(
             child: PetFeeder(),
