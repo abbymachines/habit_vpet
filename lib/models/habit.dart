5c3f4bce78dc0bed50782c50415bcc2e2798aea9
abbymachines <abby.machines@gmail.com>
2023-08-01 16:03:01 -0700

    begin to define habit model

---
+++ lib/models/habit.dart
@@ -0,0 +1,15 @@
+class Habit {
+  const Habit(
+    this.title,
+    this.description,
+    this.frequency,
+    this.habitColor,
+    this.isComplete,
+  );
+
+  final String title;
+  final String description;
+  final String frequency;
+  final String habitColor;
+  final String isComplete;
+}
---
+++ lib/widgets/habits/habit_list.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class HeartList extends StatelessWidget {
+  const HabitList(this.habits, {super.key});
+
+  final List<Habit>
+}
