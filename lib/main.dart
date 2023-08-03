75a0f333a7fe45eebc81e1c535164170e94d8fc5
abbymachines <abby.machines@gmail.com>
2023-08-03 15:06:09 -0700

    add basic color scheme and make HabitVpet() a const inside main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:habit_vpet/habit_vpet.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.red,
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true),
-      home: HabitVpet(),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+      ),
+      home: const HabitVpet(),
     ),
   );
 }
