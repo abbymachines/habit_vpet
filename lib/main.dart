8423c05e1b509205e887578d6dc2b0f3ef59657d
abbymachines <abby.machines@gmail.com>
2023-08-07 13:18:38 -0700

    add entry to blog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:habit_vpet/habit_vpet.dart';
 import 'package:habit_vpet/widgets/pet/frame_changer.dart';
 import 'package:habit_vpet/widgets/pet/pet.dart';
@@ -11,43 +12,47 @@ var kColorScheme = ColorScheme.fromSeed(
 void main() {
   // const oneSec = Duration(seconds: 1);
   // Timer.periodic(oneSec, (Timer t) => print('hi!'));
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          shape: const ContinuousRectangleBorder(
-            borderRadius: BorderRadius.zero,
-            side: BorderSide(
-              style: BorderStyle.solid,
-              color: Colors.black,
-              width: .3,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            shape: const ContinuousRectangleBorder(
+              borderRadius: BorderRadius.zero,
+              side: BorderSide(
+                style: BorderStyle.solid,
+                color: Colors.black,
+                width: .3,
+              ),
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 0,
+              vertical: 0,
             ),
           ),
-          margin: const EdgeInsets.symmetric(
-            horizontal: 0,
-            vertical: 0,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+              foregroundColor: kColorScheme.shadow,
+            ),
           ),
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-            foregroundColor: kColorScheme.shadow,
-          ),
-        ),
+        home: const HabitVpet(),
       ),
-      home: const HabitVpet(),
-    ),
-  );
+    );
+  });
 }
