ad31ec3e24aa07f065a1daf2b3bec95862be53ef
abbymachines <abby.machines@gmail.com>
2023-08-07 18:49:34 -0700

    create dummy data file

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:habit_vpet/models/habit.dart';
+
+final dummyHabits = [
+  Habit(
+    'its like going on a walk but with your cat',
+    title: 'Walk the cat',
+    frequency: 'every day',
+    habitColor: Colors.red,
+    isComplete: false,
+    isGoalMet: false,
+  ),
+  Habit(
+    'you always have homework',
+    title: 'Homework',
+    frequency: 'every weekday',
+    habitColor: Colors.yellow,
+    isComplete: true,
+    isGoalMet: true,
+  ),
+  Habit(
+    'dont forget to exercise ❣️',
+    title: 'Exercise',
+    frequency: 'once a week',
+    habitColor: Colors.blue,
+    isComplete: false,
+    isGoalMet: true,
+  ),
+  Habit(
+    'a filthy room is a filthy mind',
+    title: 'Clean your room',
+    frequency: 'twice a week',
+    habitColor: Colors.blue,
+    isComplete: true,
+    isGoalMet: true,
+  ),
+  Habit(
+    'oink oink oink oink oink',
+    title: 'Play with Piggy :)',
+    frequency: 'every day',
+    habitColor: Colors.red,
+    isComplete: true,
+    isGoalMet: true,
+  ),
+  Habit(
+    'dont get it stinky!',
+    title: 'Take out trash',
+    frequency: 'once every five days',
+    habitColor: Colors.blue,
+    isComplete: false,
+    isGoalMet: false,
+  ),
+  Habit(
+    'mmmm ice cream so good',
+    title: 'Stream',
+    frequency: 'five times a week',
+    habitColor: Colors.yellow,
+    isComplete: false,
+    isGoalMet: false,
+  ),
+  Habit(
+    'do it for piggy',
+    title: 'Clean litterbox',
+    frequency: 'every day',
+    habitColor: Colors.red,
+    isComplete: false,
+    isGoalMet: false,
+  ),
+];
---
+++ lib/providers/habits_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final habitsProvider = Provider((ref) {
+  return dummyHabits;
+});
