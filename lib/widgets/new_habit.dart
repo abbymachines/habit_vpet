39cd374ca41b516253138109a684203240baf5b5
abbymachines <abby.machines@gmail.com>
2023-08-11 13:14:20 -0700

    Revert "delete toggle habit completion method from new_habit widget"
    This reverts commit ada4aaf6d79b58137ffa3b7af68769ef21de337d.

--- lib/widgets/new_habit.dart
+++ lib/widgets/new_habit.dart
@@ -20,6 +20,10 @@ class _NewHabitState extends State<NewHabit> {
   final _isComplete = false;
   final _isGoalMet = false;
 
+  void _toggleCompletion() {
+    // set state for button here maybe?
+  }
+
   void _submitHabitData() {
     if (_titleController.text.trim().isEmpty ||
         _frequencyController.text.trim().isEmpty) {
