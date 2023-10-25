a8a142ac7f2b10751ac8e8322bbe2b4f6af235a5
abbymachines <abby.machines@gmail.com>
2023-10-25 15:54:00 -0700

    wrap Habit Vpet widget with Scaffold widget inside of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart'; // NEED TO ADD TO PUBSPEC
 
 import 'package:habit_vpet/habit_vpet.dart';
 
@@ -7,46 +8,59 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 130, 134, 12),
 );
 
+// @riverpod
+// String helloWorld(HelloWorldRef ref) {
+//   return 'Hello world';
+// }
+
 void main() {
   runApp(
     ProviderScope(
-      child: MaterialApp(
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
-          ),
-          cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            shape: const ContinuousRectangleBorder(
-              borderRadius: BorderRadius.zero,
-              side: BorderSide(
-                style: BorderStyle.solid,
-                color: Colors.black,
-                width: .3,
-              ),
-            ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 0,
-              vertical: 0,
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kColorScheme.primaryContainer),
-          ),
-          outlinedButtonTheme: OutlinedButtonThemeData(
-            style: OutlinedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-              foregroundColor: kColorScheme.shadow,
-            ),
-          ),
-        ),
-        home: const HabitVpet(),
-      ),
+      child: MyApp(),
     ),
   );
-  // });
+}
+
+class MyApp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final String value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          shape: const ContinuousRectangleBorder(
+            borderRadius: BorderRadius.zero,
+            side: BorderSide(
+              style: BorderStyle.solid,
+              color: Colors.black,
+              width: .3,
+            ),
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 0,
+            vertical: 0,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.shadow,
+          ),
+        ),
+      ),
+      home: Scaffold(body: const HabitVpet()),
+    );
+  }
 }
