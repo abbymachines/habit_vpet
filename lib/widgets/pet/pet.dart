50afe2d23f9e28dc4bc39be364ed00e23274278d
abbymachines <abby.machines@gmail.com>
2023-10-26 17:12:41 -0700

    bring back strawberry button

--- lib/widgets/pet/pet.dart
+++ lib/widgets/pet/pet.dart
@@ -3,19 +3,9 @@ import 'package:habit_vpet/widgets/heart_bar/heart_bar.dart';
 import 'package:habit_vpet/widgets/pet/frame_changer.dart';
 
 class Pet extends StatelessWidget {
-  const Pet(
-      {required this.health,
-      // required this.incrementer,
-      super.key});
+  const Pet({required this.health, super.key});
 
-  // final int currentPetFrame;
   final int health;
-  // final Function incrementer;
-
-  // @override
-  // State<Pet> createState() {
-  //   return _PetState();
-  // }
   @override
   Widget build(context) {
     return SizedBox(
@@ -24,9 +14,8 @@ class Pet extends StatelessWidget {
       child: Column(
         children: [
           HeartBar(health),
-          // FrameChanger(),
+          const FrameChanger(),
           Image.asset('assets/images/worm1.png', width: 200),
-          // TextButton(onPressed: incrementer, child: child),
         ],
       ),
     );
